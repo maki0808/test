6f3864d112aa87ace26b5c768f4a0e8dd2d7b0a7
maki0808 <79434832+maki0808@users.noreply.github.com>
2021-02-24 11:11:56 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+$(function() {
+  //スマホ（iPhone/Android/iPod）閲覧時にTEL番号のリンクが挿入されるようにする(タブレット閲覧時はPC同様にするため除外)
+  var device = window.navigator.userAgent;
+  if ((device.indexOf('iPhone') > 0 && device.indexOf('iPad') == -1) || device.indexOf('iPod') > 0 || device.indexOf('Android') > 0) {
+    $(".telep").wrap('<a href="tel:00000000000" class="tel"></a>');
+  }else{
+    alert('PCです');
+  }
+  $('tel').click();
+  console.log("aaaaa");
+});
